ac1cdf8de99fb8cca31f304d541441dc1784efa0
njabuloszulu <njabuloszulu@gmail.com>
2019-03-12 14:43:38 +0200

    first commit

---
+++ statements.js
@@ -0,0 +1,16 @@
+var name = "njabulo";
+var age = 24;
+var both = (name + age);
+
+var randomNumber1 = 12;
+var randomNumbr2 = 34;
+var addRandomNumbers = randomNumber1 + randomNumbr2;
+
+var heatRate1 = 100;
+var heatRate2 = heatRate1 * 2;
+var heatRate3 = heatRate2 - 15;
+var overallHeatRate = heatRate1 + heatRate2 + heatRate3; 
+console.log(overallHeatRate + '\n');
+// console.log( );
+console.log(addRandomNumbers);
+console.log(both);
