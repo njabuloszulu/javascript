9760ea4bffc46dd61fef9845f71b040366ca6bd0
njabuloszulu <njabuloszulu@gmail.com>
2019-03-13 13:47:53 +0200

    Testing functions

---
+++ tryingOutFunctiocs.js
@@ -0,0 +1,13 @@
+
+var multiplication = multiply(5,100);
+var addition = adding(10,20)
+
+    function multiply(p1, p2){
+        return p1 * p2;
+}
+    function adding(value1,value2){
+        return value1 + value2;
+}
+
+console.log(multiplication);
+console.log( addition);
