31d9a9ae8a5bc0fce1e44d3abe95acb77fc05742
njabuloszulu <njabuloszulu@gmail.com>
2020-07-03 15:25:31 +0200

    finding the unique number

---
+++ Find-The-Unique-Number.js
@@ -0,0 +1,20 @@
+// findUniqueNumber = (iterable) =>{
+//     let uniqueNumber = []
+//     let similarNumber = []
+
+//     for (var i = 0;i < iterable.length;i ++){
+//         let curr = [i]
+//         let next = [i +1]
+//         if(curr == next){
+//             similarNumber.push(curr)
+//         }
+//         else{
+//             uniqueNumber.push(curr)
+//         }
+//     }
+//     return similarNumber
+// }
+
+// console.log(findUniqueNumber("AAAABAAA"))
+
+
